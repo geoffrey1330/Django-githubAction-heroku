b4e034be47ef0b21c151dd1c7cca8d9e38a3abd7
Geoffrey Israel <israelgeoffrey13@gmail.com>
2022-04-01 23:25:20 +0100

    first commit

--- contacts/tests.py
+++ contacts/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from . models import Contact
 
-# Create your tests here.
+
+class ModelTesting(TestCase):
+
+    def setUp(self):
+        self.contact = Contact.objects.create(full_name='michael', relationship='brother',
+         email='michael1234@gmail.com',phone_number="+234817779056",address="lagos,Nigeria")
+
+    def test_contact_model(self):
+        d = self.contact
+        self.assertTrue(isinstance(d, Contact))
+        self.assertEqual(str(d), 'michael')
